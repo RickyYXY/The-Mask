8d43c6a370707dabf3b58296cf8843da73a1b3d4
RickyYXY <yuxingyuan@gmail.com>
2021-09-18 16:35:41 +0800

    sefa api v1.0

---
+++ BackEnd/Sefa/api.py
@@ -0,0 +1,51 @@
+"""SeFa."""
+
+import os
+import argparse
+from tqdm import tqdm
+import numpy as np
+
+import torch
+
+from models import parse_gan_type
+from utils import to_tensor
+from utils import postprocess
+from utils import load_generator
+from utils import factorize_weight
+
+
+def synthesize(generator, gan_type, codes):
+    """Synthesizes images with the give codes."""
+    if gan_type == 'pggan':
+        images = generator(to_tensor(codes))['image']
+    elif gan_type in ['stylegan', 'stylegan2']:
+        images = generator.synthesis(to_tensor(codes))['image']
+    images = postprocess(images)
+    return images
+
+
+def code_to_img_api(code, layer_idx, num_semantics, step,
+                    start_distance=-3, end_distance=3, step_num=11, model_name='pggan_celebahq1024'):
+    """
+    code: code for img
+    layer_idx: 'all', '0-1', '2-5', '6-13'
+    num_semantics: count of the chosen semantics
+    step: the value index of distance(0~10), depend on step_num
+    """
+    generator = load_generator(model_name)
+    gan_type = parse_gan_type(generator)
+    layers, boundaries, values = factorize_weight(generator, layer_idx)
+
+    distances = np.linspace(start_distance, end_distance, step_num)
+    num_sem = num_semantics
+
+    temp_code = code.copy()
+    for sem_id in tqdm(range(num_sem), desc='Semantic ', leave=False):
+        boundary = boundaries[sem_id:sem_id + 1]
+        d = distances[step]
+        if gan_type == 'pggan':
+            temp_code += boundary * d
+        elif gan_type in ['stylegan', 'stylegan2']:
+            temp_code[:, layers, :] += boundary * d
+    new_images = synthesize(generator, gan_type, temp_code)
+    return new_images
