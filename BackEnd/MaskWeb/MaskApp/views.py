c6e5480200da10e7dcbc4cd5e627d9c8c239219e
wycwcq <1193032407@qq.com>
2021-10-10 19:41:03 +0800

    Add initial view

--- BackEnd/MaskWeb/MaskApp/views.py
+++ BackEnd/MaskWeb/MaskApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def FaceGenerate(request):
+    face_text = request
+    return HttpResponse("Hello, world. You're at the MaskApp Test.")
